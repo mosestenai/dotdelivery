2ecc82ac993d60520d9ad28fd002cf71d8755730
Tenai <kipronotena@gmail.com>
2022-06-04 15:47:17 +0300

    beta version

--- src/pages/Account/fetchfoods.js
+++ src/pages/Account/fetchfoods.js
@@ -28,6 +28,9 @@ const Fetchfoods = () => {
     const [scroll, setscroll] = useState('auto');
     const [openPanel, setOpenPanel] = useState(true);
 
+    const [longitude, setlongitude] = useState('');
+    const [latitude, setlatitude] = useState('');
+
 
     const [restaurants, setrestaurants] = useState([]);
     const [menu, setmenu] = useState([]);
@@ -37,6 +40,7 @@ const Fetchfoods = () => {
 
     const restaurantscollectionRef = collection(db, "branches");
     const menucollection = collection(db, "menus")
+
 
     useEffect(() => {
         const getRestaurants = async () => {
@@ -52,7 +56,43 @@ const Fetchfoods = () => {
 
         getMenus();
         getRestaurants();
+        getlocation();
     }, [])
+
+    
+    const getlocation = () => {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log(position.coords.latitude)
+                setlatitude(position.coords.latitude)
+                setlongitude(position.coords.longitude)
+            },
+            function (error) {
+                alert(error.message + ". PLease allow location to continue");
+            }
+        );
+
+    }
+
+    function calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      var R = 6371; // km
+      var dLat = toRad(lat2-lat1);
+      var dLon = toRad(lon2-lon1);
+      var lat1 = toRad(lat1);
+      var lat2 = toRad(lat2);
+
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c;
+      return Math.round(d);
+    }
+    function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
+
 
 
 
@@ -144,8 +184,8 @@ const Fetchfoods = () => {
                                 <img src={val.branchImg} /><br />
 
                                 <div className="foodrestaurant">{val.restaurant}</div><br />
-
-                                <div style={{ display: "contents", }} ><FaMapMarker color="#ff9334" /> {val.branchAddress.substring(0, 25)}...</div>
+                                <div style={{ display: "contents", }} ><FaMapMarker color="#ff9334" /> {val.branchAddress.substring(0, 25)}...</div><br />
+                               <div style={{fontSize:10,fontFamily:"cursive"}}> ({calcCrow(val.latitude,val.longitude,latitude,longitude)}km away)</div>
                                 {/* <div className="fooddeliveryprice">{val.restCategories[0]}</div>*/}
                             </div>
                         )
