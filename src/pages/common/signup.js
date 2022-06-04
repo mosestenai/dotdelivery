dfde457bc65f4e1694ff1bb2991ea2c2bf484ac3
Tenai <kipronotena@gmail.com>
2022-06-04 08:22:17 +0300

    beta version

--- src/pages/common/signup.js
+++ src/pages/common/signup.js
@@ -11,6 +11,9 @@ const Signup = () => {
 
     const [email, setemail] = useState('');
     const [phone, setphone] = useState('');
+    const [value1, setvalue1] = useState('');
+    const [value2, setvalue2] = useState('');
+    const [value3, setvalue3] = useState('');
     const [password1, setpassword1] = useState('');
     const [password2, setpassword2] = useState('');
     const [loading, setloading] = useState('');
@@ -19,19 +22,10 @@ const Signup = () => {
 
     const [message, setmessage] = useState('');
 
-    const [code1, setcode1] = useState('');
-    const [code2, setcode2] = useState('');
-    const [code3, setcode3] = useState('');
-    const [code4, setcode4] = useState('');
-
     document.body.style.overflow = 'auto';
 
-
-
-
-
     const [first, setfirst] = useState(true);
-    const [verify, setverify] = useState(false);
+   const [second, setsecond] = useState(false);
     const [passwords, setpasswords] = useState(false);
     const [overlaystatus, setoverlaystatus] = useState(false);
 
@@ -42,28 +36,37 @@ const Signup = () => {
     useEffect(() => {
 
     }, []);
-    const inputRef2 = React.createRef();
-    const inputRef3 = React.createRef();
-    const inputRef4 = React.createRef();
+  
 
 
 
 
-    const getcode = () => {
-        /*  const valemail = email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    const checkfield = () => {
+          const valemail = value1.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
           if (valemail) {
-              console.log("valid email")
+              setemail(value1)
+              setvalue2("phone number")
           } else {
-              console.log("invalid email")
-          }*/
+            setphone(value1)
+            setvalue2("email")
+          }
         setfirst(false)
-        setverify(true)
+        setsecond(true)
+      
     }
 
-    const verifycode = () => {
-        setverify(false)
-        setpasswords(true)
+    const checksecondfield = () =>{
+        const valemail = value3.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        if (valemail) {
+            setemail(value3)
+        } else {
+          setphone(value3)
+        }
+      setpasswords(true)
+      setsecond(false) 
     }
+
+   
 
     const handlesignup = async () => {
 
@@ -103,12 +106,12 @@ const Signup = () => {
                         </p>
                         <button className="confirmbuttonyes" onClick={() => {
                             setoverlaystatus(false)
-                            setverify(false)
+                            setsecond(false)
                             setfirst(true)
                         }}>Yes</button><br />
                         <button className="confirmbuttonno" onClick={() => {
                             setoverlaystatus(false)
-                            setverify(true)
+                            setsecond(true)
                         }}>No</button>
 
                     </div>
@@ -138,7 +141,7 @@ const Signup = () => {
             {first &&
                 <div className="loginform">
                     <p className="loginquestion">What's your phone number or <br /> email?</p>
-                    <input type="text" className="logininput" value={email} onChange={(e) => setemail(e.target.value)} />
+                    <input type="text" className="logininput" value={value1} onChange={(e) => setvalue1(e.target.value)} />
                     <div className="continuedetails">
                         By proceeding, you consent to get calls, WhatsApp or<br />
                         SMS messages, including by automated means, from Dot<br /> delivery
@@ -146,11 +149,11 @@ const Signup = () => {
                     </div>
 
                     <div className="nextbutton"
-                        onClick={getcode}
+                        onClick={checkfield}
                         style={{
-                            color: !email ? "gray" : "white",
-                            backgroundColor: !email ? "white" : "#ff9334",
-                            cursor: !email ? "not-allowed" : "pointer"
+                            color: !value1 ? "gray" : "white",
+                            backgroundColor: !value1 ? "white" : "#ff9334",
+                            cursor: !value1 ? "not-allowed" : "pointer"
                         }}>Next &nbsp;<FaArrowRight /></div>
                     <div className="continuedetails">
                         This site is protected by reCAPTCHA and the Google Privacy Policy
@@ -159,74 +162,31 @@ const Signup = () => {
                     </div>
                 </div>
             }
-            {verify &&
+            {second &&
                 <div className="loginform">
-                    <p className="loginquestion">Enter the four digit code sent to you <br />
-                        at {email ? email : phone}
-                    </p>
-                    <input type="text" className="codeinput" autoFocus value={code1} onChange={(e) => {
-                        const re = /^[0-9\b]+$/;
-                        if (e.target.value === '' || re.test(e.target.value)) {
-                            setcode1(e.target.value)
-                            if (e.target.value) {
-                                if (inputRef2.current) { inputRef2.current.focus() }
-                            }
-                        }
-                    }} />
-                    <input type="text" className="codeinput" ref={inputRef2} value={code2} onChange={(e) => {
-                        const re = /^[0-9\b]+$/;
-                        if (e.target.value === '' || re.test(e.target.value)) {
-                            setcode2(e.target.value)
-                            if (e.target.value) {
-                                if (inputRef3.current) { inputRef3.current.focus() }
-                            }
-                        }
-                    }} />
-                    <input type="text" className="codeinput" ref={inputRef3} value={code3} onChange={(e) => {
-                        const re = /^[0-9\b]+$/;
-                        if (e.target.value === '' || re.test(e.target.value)) {
-                            setcode3(e.target.value)
-                            if (e.target.value) {
-                                if (inputRef4.current) { inputRef4.current.focus() }
-                            }
-                        }
+                    <p className="loginquestion">What's your {value2} ?</p>
+                    <input type="text" className="logininput" value={value3} onChange={(e) => setvalue3(e.target.value)} />
+                    <div className="continuedetails">
+                        By proceeding, you consent to get calls, WhatsApp or<br />
+                        SMS messages, including by automated means, from Dot<br /> delivery
+                        and its affiliates to the number provided.
+                    </div>
 
-                    }} />
-                    <input type="text" className="codeinput" ref={inputRef4} value={code4} onChange={(e) => {
-                        const re = /^[0-9\b]+$/;
-                        if (e.target.value === '' || re.test(e.target.value)) {
-                            setcode4(e.target.value)
-                        }
-                    }} />
-                    <br />
-
-                    <button className="resendbutton"
-                        onClick={() => {
-                            setshowback(false)
-                            setoverlaystatus(true)
-                        }}
-                    >I did't receive a code</button>
-
-                    <div className="navformbuttons">
-                        <div className="navformbackbutton"
-                            onClick={() => {
-                                setoverlaystatus(true)
-                                setshowback(true)
-                            }}
-                        > &nbsp;<FaArrowLeft />
-                        </div>
-
-                        <div className="navformnextbutton"
-                            onClick={verifycode}
-                            style={{
-                                color: !code1 || !code2 || !code3 || !code4 ? "gray" : "white",
-                                backgroundColor: !code1 || !code2 || !code3 || !code4 ? "white" : "#ff9334",
-                                cursor: !code1 || !code2 || !code3 || !code4 ? "not-allowed" : "pointer"
-                            }}>Next &nbsp;<FaArrowRight />
-                        </div>
+                    <div className="nextbutton"
+                        onClick={checksecondfield}
+                        style={{
+                            color: !value3 ? "gray" : "white",
+                            backgroundColor: !value3 ? "white" : "#ff9334",
+                            cursor: !value3 ? "not-allowed" : "pointer"
+                        }}>Next &nbsp;<FaArrowRight /></div>
+                    <div className="continuedetails">
+                        This site is protected by reCAPTCHA and the Google Privacy Policy
+                        and Terms of Service apply.
+                        <br /><br />
                     </div>
                 </div>
             }
+           
             {
                 passwords &&
                 <div className="loginform">
