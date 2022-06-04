0b50885646108514f166733d36d9dab869e1095e
Tenai <kipronotena@gmail.com>
2022-06-04 09:00:23 +0300

    beta version

--- src/pages/common/signup.js
+++ src/pages/common/signup.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "./../../firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setUserSession } from "./../Utils/common";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 
 const Signup = () => {
@@ -17,6 +19,8 @@ const Signup = () => {
     const [password1, setpassword1] = useState('');
     const [password2, setpassword2] = useState('');
     const [loading, setloading] = useState('');
+
+    const [value, setValue] = useState()
 
     const navigate = useNavigate();
 
@@ -41,39 +45,36 @@ const Signup = () => {
 
 
 
-    const checkfield = () => {
-        const valemail = value1.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        if (valemail) {
-            setemail(value1)
-            setvalue2("phone number")
-            setfirst(false)
-            setsecond(true)
+    const checkphone = () => {
+        seterror(null)
+        if (!phone) {
+
         } else {
-            setphone(value1)
-            setvalue2("email")
-            setfirst(false)
-            setsecond(true)
+            console.log(phone)
+            var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+            if (re.test(phone)) {
+                setfirst(false)
+                setsecond(true)
+            } else {
+                seterror("invalid phone number")
+
+            }
         }
     }
 
-    const checksecondfield = () => {
-        const valemail = value3.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        if (valemail) {
-            setemail(value3)
-            setpasswords(true)
-            setsecond(false)
+    const checkemail = () => {
+        seterror(null)
+        if (!email) {
+
         } else {
-            var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-            if (re.test(value3)) {
-                setphone(value3)
+            const valemail = email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            if (valemail) {
                 setpasswords(true)
                 setsecond(false)
             } else {
-                seterror("invalid phone number")
-                setpasswords(true)
-                setsecond(false)
-            }
 
+                seterror("Invalid email address")
+            }
         }
 
     }
@@ -152,9 +153,14 @@ const Signup = () => {
 
             {first &&
                 <div className="loginform">
-                    <p className="loginquestion">What's your phone number or <br /> email?</p>
-                    <input type="text" className="logininput" value={value1} onChange={(e) => setvalue1(e.target.value)} />
-                    <div className="continuedetails">
+                    <p className="loginquestion">What's your phone number?</p>
+                    <PhoneInput
+                        placeholder="Enter phone number"
+                        value={phone}
+                        defaultCountry="BW"
+                        onChange={setphone} />
+
+                     <div className="continuedetails">
                         By proceeding, you consent to get calls, WhatsApp or<br />
                         SMS messages, including by automated means, from Dot<br /> delivery
                         and its affiliates to the number provided.
@@ -162,11 +168,11 @@ const Signup = () => {
                     <div style={{ color: "red" }}>{error}</div>
 
                     <div className="nextbutton"
-                        onClick={checkfield}
+                        onClick={checkphone}
                         style={{
-                            color: !value1 ? "gray" : "white",
-                            backgroundColor: !value1 ? "white" : "#ff9334",
-                            cursor: !value1 ? "not-allowed" : "pointer"
+                            color: !phone ? "gray" : "white",
+                            backgroundColor: !phone ? "white" : "#ff9334",
+                            cursor: !phone ? "not-allowed" : "pointer"
                         }}>Next &nbsp;<FaArrowRight /></div>
                     <div className="continuedetails">
                         This site is protected by reCAPTCHA and the Google Privacy Policy
@@ -177,17 +183,17 @@ const Signup = () => {
             }
             {second &&
                 <div className="loginform">
-                    <p className="loginquestion">What's your {value2} ?</p>
-                    <input type="text" className="logininput" value={value3} onChange={(e) => setvalue3(e.target.value)} />
+                    <p className="loginquestion">What's your email address ?</p>
+                    <input type="text" className="logininput" value={email} onChange={(e) => setemail(e.target.value)} />
                     <div className="continuedetails">
-                        By proceeding, you consent to get calls, WhatsApp or<br />
-                        SMS messages, including by automated means, from Dot<br /> delivery
-                        and its affiliates to the number provided.
+                        By proceeding, you consent to emails, <br />
+                        including by automated means, from Dot<br /> delivery
+                        and its affiliates to the address provided.
                     </div>
                     <div style={{ color: "red" }}>{error}</div>
 
                     <div className="nextbutton"
-                        onClick={checksecondfield}
+                        onClick={checkemail}
                         style={{
                             color: !value3 ? "gray" : "white",
                             backgroundColor: !value3 ? "white" : "#ff9334",
